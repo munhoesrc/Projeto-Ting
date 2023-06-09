6d8269cf4c9e4c382c7d372c16702ac9aa809124
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-06-09 13:06:09 -0300

    feat: implementando a função process no módulo file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,31 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    # """Aqui irá sua implementação"""
+    processed_files = set([item["nome_do_arquivo"] for item in instance.queue])
+
+    if path_file in processed_files:
+        print(
+            f"Arquivo {path_file} já foi processado."
+            "Ignorando.",
+            file=sys.stdout
+        )
+        return
+
+    lines = txt_importer(path_file)
+
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    instance.enqueue(file_data)
+
+    print(file_data)
 
 
 def remove(instance):
